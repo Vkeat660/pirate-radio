3806f21aedc7838d55d35965e93b3df542bfe513
Vivian Keating <vkeat660@gmail.com>
2016-01-31 16:23:43 -0500

    Added mongoDB

--- pirate-radio.jsx
+++ pirate-radio.jsx
@@ -1,11 +1,12 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // Code that is executed on the client only
-  
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
 
   });
-
 
 }
