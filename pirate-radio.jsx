fc51a5b099750c9d0f9d694d0991bface95fb34f
Vivian Keating <vkeat660@gmail.com>
2016-02-01 21:30:04 -0500

    Finally got fucking video working

--- pirate-radio.jsx
+++ pirate-radio.jsx
@@ -13,6 +13,38 @@ if (Meteor.isClient) {
     React.render(<App />, document.getElementById("render-target"));
 
   });
+
+
+  // YouTube API will call onYouTubeIframeAPIReady() when API ready.
+    // Make sure it's a global variable.
+    onYouTubeIframeAPIReady = function () {
+
+        // New Video Player, the first argument is the id of the div.
+        // Make sure it's a global variable.
+        player = new YT.Player("player", {
+
+            height: "400", 
+            width: "600", 
+
+            // videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+            videoId: "LdH1hSWGFGU", 
+
+            // Events like ready, state change, 
+            events: {
+
+                onReady: function (event) {
+
+                    // Play video when player ready.
+                    event.target.playVideo();
+                }
+
+            }
+
+        });
+
+    };
+
+    YT.load();
 }
 
 if (Meteor.isServer) {
